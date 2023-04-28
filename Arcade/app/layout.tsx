b16adeb1e2598c928b65ceff610f3a7114a51113
Kaival <87368466+Kaival1212@users.noreply.github.com>
2023-04-28 23:48:52 +0100

    Update layout.tsx

--- Arcade/app/layout.tsx
+++ Arcade/app/layout.tsx
@@ -7,7 +7,7 @@ export default function RootLayout({children,}: {children: React.ReactNode}) {
     <html lang="en">
       <body>
         <div className=' bg-gray-500 p-5 flex justify-between'>
-          <Image src={logo} height={60} width={270}></Image>
+          <Image src={logo} alt="loading" height={60} width={270}></Image>
           <div className='mt-2'>
           <button className=' bg-red-600 mx-3 h-9 w-20 rounded-xl hover:bg-red-800'>Cart</button>
           <button className=' bg-red-600 mx-3 h-9 w-20 rounded-xl hover:bg-red-800'>Account</button>
