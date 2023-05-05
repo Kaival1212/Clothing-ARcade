8c0edbee92bcd959f1741f9d86bf46419f0ed886
Kaival <87368466+Kaival1212@users.noreply.github.com>
2023-05-05 22:24:41 +0100

    edit

--- Arcade/app/admin/home/products/page.tsx
+++ Arcade/app/admin/home/products/page.tsx
@@ -1,45 +1,55 @@
 "use client"
-
+// Importing necessary packages and components
 import Link from "next/link";
 import { useEffect, useState } from "react";
 
-export default  function products() {
+// Defining a functional component named 'products'
+export default function products() {
+  
+  // Defining a state variable named 'Data' and a function to update it named 'SetData'
+  const [Data, SetData] = useState<{ product_id: string, product_name: string, product_des: string, product_price: number, product_category: string, product_images: string[] }[]>([]);
 
-  const [Data,SetData]= useState([])
-
+  // Defining an asynchronous function named 'fetchData' to fetch data from the API endpoint
   const fetchData = async () => {
-    const product_data= await fetch("/api/Backend/Mongodb", { method: "GET" });
+    // Fetching data from the specified endpoint using the 'fetch' method
+    const product_data = await fetch("/api/Backend/Mongodb", { method: "GET" });
+    // Parsing the response data into a JSON object using the 'json' method
     const json_data = await product_data.json(); // await the result of json() method
+    // Updating the 'Data' state variable with the fetched data
     SetData(json_data);
   };
 
+  // Defining a useEffect hook to fetch data from the API endpoint when the component mounts
   useEffect(() => {
     fetchData();
   }, []);
-  console.log(Data)
-  
 
+  // Rendering the JSX content to be displayed by the component
   return (
     <div className="h-screen flex flex-col">
+      {/* Linking to the add new product page */}
       <Link href={"/admin/home/products/new"}>
         <button className="bg-[#4B0082] text-white px-6 py-2 mt-10 rounded-xl mx-5">
           Add New Product
         </button>
       </Link>
       <div>
-      {Data.length !== 0 && Data.map((product) => (
-  <div key={product.product_id}>
-    <h1>{product.product_name}</h1>
-    <p>{product.product_des}</p>
-    <p>{product.product_price}</p>
-    <p>{product.product_category}</p>
-    <div className="flex">
-    {product.product_images.map((image, index) => (
-      <img key={index} src={image} alt={`Image ${index}`} height={100} width={100}/>
-    ))}
-    </div>
-  </div>
-))}      </div>
+        {/* Rendering the products data fetched from the API */}
+        {Data.length !== 0 ? Data.map((product) => (
+          <div key={product.product_id}>
+            <h1>product_name:{product.product_name}</h1>
+            <p>product_des:{product.product_des}</p>
+            <p>product_price:{product.product_price}</p>
+            <p>product_category:{product.product_category}</p>
+            <div className="flex">
+              {/* Rendering the product images */}
+              {product.product_images.map((image, index) => (
+                <img key={index} src={image} alt={`Image ${index}`} height={100} width={100} />
+              ))}
+            </div>
+          </div>
+        )) : <h1>Loading....</h1>} {/* Displaying a loading message while the data is being fetched */}
+      </div>
     </div>
   );
 }
--- Arcade/app/api/Backend/Mongodb/route.tsx
+++ Arcade/app/api/Backend/Mongodb/route.tsx
@@ -1,44 +1,47 @@
+// Importing necessary packages and modules
 import { NextRequest, NextResponse } from 'next/server';
 import { MongoClient } from 'mongodb';
 
+// Defining an asynchronous function to handle HTTP POST requests
 export async function POST(req: Request) {
-    const data = await req.json();
+    const data = await req.json(); // Parsing the incoming request data as JSON
 
-    if (data.product_id==""){
-      return NextResponse.json("error no data",{ status: 406 })
-    }
-    else{
-        if (data.product_images.length==0){
-          return NextResponse.json("error no images", {status: 406})
-        }
-        else{
-          const connectionString = process.env.MONGODB_URI || '';
-          const client = new MongoClient(connectionString);
-          await client.connect();
-          const db = client.db("ARclothing")
-          const col = db.collection("products");
-          await col.insertOne(data)
+    // Checking if the product_id field in the request data is empty
+    if (data.product_id == "") {
+        return NextResponse.json("error no data", { status: 406 }) // Returning a JSON response with a 406 status code if product_id is empty
+    } else {
+        // Checking if the product_images array in the request data is empty
+        if (data.product_images.length == 0) {
+            return NextResponse.json("error no images", { status: 406 }) // Returning a JSON response with a 406 status code if product_images is empty
+        } else {
+            // Connecting to the MongoDB database and inserting the request data into the 'products' collection
+            const connectionString = process.env.MONGODB_URI || ''; // Getting the MongoDB connection string from the environment variables
+            const client = new MongoClient(connectionString);
+            await client.connect(); // Connecting to the MongoDB database
+            const db = client.db("ARclothing"); // Selecting the 'ARclothing' database
+            const col = db.collection("products"); // Selecting the 'products' collection
+            await col.insertOne(data); // Inserting the request data into the collection
         }
         
-        
-      return NextResponse.json('data added successfully', { status: 200 });
+        // Returning a JSON response with a 200 status code if the data was successfully added to the collection
+        return NextResponse.json('data added successfully', { status: 200 });
     }
 }
 
-
+// Defining an asynchronous function to handle HTTP GET requests
 export async function GET() {
-  const connectionString = process.env.MONGODB_URI || '';
-  const client = new MongoClient(connectionString);
+    const connectionString = process.env.MONGODB_URI || ''; // Getting the MongoDB connection string from the environment variables
+    const client = new MongoClient(connectionString);
 
-  try {
-    await client.connect();
-    const db = await client.db("ARclothing");
-    const col = await db.collection("products");
-    const data = await col.find().toArray();
-    return NextResponse.json(data);
-  } catch (error) {
-    return NextResponse.json({ error: "error" });
-  } finally {
-    await client.close();
-  }
+    try {
+        await client.connect(); // Connecting to the MongoDB database
+        const db = await client.db("ARclothing"); // Selecting the 'ARclothing' database
+        const col = await db.collection("products"); // Selecting the 'products' collection
+        const data = await col.find().toArray(); // Fetching all documents from the collection and converting them to an array
+        return NextResponse.json(data); // Returning a JSON response with the fetched data
+    } catch (error) {
+        return NextResponse.json({ error: "error" }); // Returning a JSON response with an error message if an error occurs
+    } finally {
+        await client.close(); // Closing the MongoDB client connection
+    }
 }
